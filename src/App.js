d50dd6b5fc51334ade2a779cc83e9fac128febb5
Dulan Fernando <dulanfernando0719@gmail.com>
2025-05-21 23:37:54 +0530

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { MyNavbar } from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MyNavbar />
       <h1>Yo</h1>
     </div>
   );
