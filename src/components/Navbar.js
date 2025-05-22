2917d85203bf17167218e10311a2619313e01aba
Dulan Fernando <dulanfernando0719@gmail.com>
2025-05-22 22:44:00 +0530

    Navbar Styling - 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,18 @@ export const MyNavbar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Thudawa Child Development Center</Navbar.Brand>
+        <Navbar.Brand href="#home">Thudawa Child <span>Development Center</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="pages\Aboutus.js">About Us</Nav.Link>
+            <Nav.Link href="pages\Background.js">Background</Nav.Link>
+            <Nav.Link href="#link">Behind Us</Nav.Link>
+            <Nav.Link href="#link">Achievements</Nav.Link>
+            <Nav.Link href="#link">Events</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+            <Nav.Link href="#link">Donate Us</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
