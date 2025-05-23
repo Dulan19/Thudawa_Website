387da83d602a9d346c6e7f00a2763e5cbc39550b
Dulan Fernando <dulanfernando0719@gmail.com>
2025-05-23 23:11:15 +0530

    Fix Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 //import logo from '../../../public/logo192.png'; // Adjust if incorrect
 
