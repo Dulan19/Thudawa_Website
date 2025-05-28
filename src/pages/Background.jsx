11f40babfcb63141f42828ce360983124b14b41b
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-05-28 20:27:55 +0530

    created Background.jsx
    just simply imported  navigation bar and footer.

---
+++ src/pages/Background.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from '../components/Navbar/Navbar.jsx'; 
+import Footer from '../components/Footer/Footer.jsx';
+import "../App.css"
+
+
+
+function Background() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Background
