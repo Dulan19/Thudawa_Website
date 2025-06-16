cbf39cd0b84518cfc492e26fd11523870f183b24
Dulan Fernando <dulanfernando0719@gmail.com>
2025-06-16 23:36:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
                 <div className="Events-container">
                   {/* Events boxes */}
                   <div className="Events-box">
-                    <h2>Our Vision</h2>
+                    <h2>Bakthi Geetha</h2>
                     <p>
                       Our vision is to put an end to the sufferings of innocent children who are being tampered by social
                       imbalances and injustice; to recreate their childhoods through justice and social equality and through
