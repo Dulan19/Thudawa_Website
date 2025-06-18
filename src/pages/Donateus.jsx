fb1d8f24009c9edd6fe3d3d73381728ce74abd8f
Dulan Fernando <dulanfernando0719@gmail.com>
2025-06-18 19:01:18 +0530

    Update Donateus.jsx

--- src/pages/Donateus.jsx
+++ src/pages/Donateus.jsx
@@ -12,19 +12,19 @@ const Donateus = () => {
         <h1>Support Our Cause</h1>
         <p>Your contributions help us provide better care and facilities and Quality Education 
             for the children at Tudawe Child Development Center.</p>
-        <h2>How to Donate</h2>
+        <h1>How to Donate</h1>
         <ul>
           <li>Bank Transfer: 
-            <strong>Account Name:</strong> Tudawe Children Development Centre<br />
-            <strong>Bank:</strong> Bank of Ceylon (7010)<br />
-            <strong>Account Number:</strong> 0003699120<br />
-            <strong>Branch:</strong> Wadduwa Branch (584)
+            Account Name: Tudawe Children Development Centre<br />
+            Bank: Bank of Ceylon (7010)<br />
+            Account Number: 0003699120<br />
+            Branch: Wadduwa Branch (584)
           </li>
           <li>Online Payment: Visit our website and click on the "Donate" button.</li>
           <li>In-Kind Donations: We accept toys, books, and educational materials.</li>
         </ul>
-        <h2>Contact Us for More Information</h2>
-        <button className ="contactus-button">Contact Us</button>
+        <h1>Contact Us for More Information</h1>
+        <button className ="contactus-button" onClick={() => window.location.href = '/events'}>Contact Us</button>
         </div>
         </div>
         <Footer />
