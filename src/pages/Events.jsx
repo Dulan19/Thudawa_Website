d243633c5e48424e5db3c7bdfd0e6c11f8bbefbd
Dulan Fernando <dulanfernando0719@gmail.com>
2025-06-29 21:00:09 +0530

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -17,7 +17,9 @@ const Events = () => {
         <div className="section-container">
           <div className="intro-section">
             <h2>Bakthi Geetha - 2025</h2>
-            <p>space for the image
+            <p>On the occasion of Poson Poya Day, with the valuable support of Mrs. Palika Darmawickrama, 
+              The Music Teacher of the Thudawa Child Development Centre, and the dedicated efforts of the Centre’s committee, 
+              The Thudawa Child Development Centre successfully presented “Bakthi Gee 2025” together with its children
             </p>
           </div>
         </div>
