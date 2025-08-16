021499be13641767a4315e7d20c561385d001344
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-08-16 23:44:52 +0530

    c

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -309,7 +309,7 @@ function Aboutus() {
                   </div>
                   <div className="routine-item">
                     <div className="day-img-container day-img-4"></div>
-                    <span>Shared meals </span>
+                    <span>Shared meals c</span>
                   </div>
                 </div>
               </div>
