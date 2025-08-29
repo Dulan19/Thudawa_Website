f0f97ddf66f7ddde724fd206311cb8d734672c2c
Dulan Fernando <dulanfernando0719@gmail.com>
2025-08-29 22:22:40 +0530

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -174,12 +174,11 @@ const Events = () => {
                     carouselId="shrine-2023"
                     images={[
                       { src: "assets/2023 - buduge sadeema/2023-shrine (1).jpeg", alt: "shrine1" },
-                      { src: "assets/2023 - buduge sadeema/2023-shrine (2).jpeg", alt: "shrine7" },
-                      { src: "assets/2023 - buduge sadeema/2023-shrine (3).jpeg", alt: "shrine6" },
+                      { src: "assets/2023 - buduge sadeema/2023-shrine (2).jpg", alt: "shrine7" },
+                      { src: "assets/2023 - buduge sadeema/2023-shrine (3).jpg", alt: "shrine6" },
                       { src: "assets/2023 - buduge sadeema/2023-shrine (4).jpeg", alt: "shrine5" },
                       { src: "assets/2023 - buduge sadeema/2023-shrine (5).jpeg", alt: "shrine4" },
                       { src: "assets/2023 - buduge sadeema/2023-shrine (6).jpeg", alt: "shrine3" },
-                      { src: "assets/2023 - buduge sadeema/2023-shrine (7).jpeg", alt: "shrine2" },
                     ]}
                   />
               <p> The new Buddha Shrine at the Tudawe Child Development Centre was officially opened in a special ceremony in 
