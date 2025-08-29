d2bc9fb801e5437b8cf2cd5b9efb2b2536c9b8b9
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-08-29 21:20:13 +0530

    8

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -83,7 +83,7 @@ function Aboutus() {
                   Tudawe historical house is located in the beautiful village area. The Tudawe Children Development
                   Center premises gathered around the parental House and Property which has enough space for buildings
                   as well as playgrounds. We have to remember Tudawe Family and other initiators who encourage us to
-                  donate this nearly half-arced property for making a better world for children.
+                  donate this nearly half-arced property for making a better world for children.1234567
                 </p>
               </div>
             </div>
