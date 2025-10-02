fd1261e38e650c2e0f8c17ba9a7e51c3676117eb
SamadhiViduravi <167635543+SamadhiViduravi@users.noreply.github.com>
2025-10-02 19:33:44 +0530

    8

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -713,7 +713,7 @@ function Aboutus() {
               <p>
                 At Tudawe Children Development Center, every day is an opportunity to nurture hope, foster dreams, and
                 create futures filled with possibility. Together, we continue this journey of transformation, one child
-                at a time.
+                at a time.1234567
               </p>
             </div>
           </div>
